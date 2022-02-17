cdc10af795f80b8cdd98d4fd0100e73cc39c8fc3
Daniel Aguilar <dacmail@gmail.com>
2022-02-17 15:59:48 +0100

    Header

--- wp-content/themes/adelanteandalucia/resources/views/partials/header.blade.php
+++ wp-content/themes/adelanteandalucia/resources/views/partials/header.blade.php
@@ -1,14 +1,16 @@
 <header class="c-header">
-  <?php the_custom_logo(); ?>
-  @if (has_nav_menu('primary_navigation'))
-    <a href="#" class="c-header__menu-toggle js-menu-toggle">
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="bar"></span>
-      <span class="text">{{ esc_html__('Menú', 'adelanteandalucia') }}</span>
-    </a>
-    <nav class="nav-primary js-menu">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => false]) !!}
-    </nav>
-  @endif
+  <div class="c-header__wrapper o-container">
+    <?php the_custom_logo(); ?>
+    @if (has_nav_menu('primary_navigation'))
+      <a href="#" class="c-header__menu-toggle js-menu-toggle">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="text">{{ esc_html__('Menú', 'adelanteandalucia') }}</span>
+      </a>
+      <nav class="nav-primary js-menu">
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => false]) !!}
+      </nav>
+    @endif
+  </div>
 </header>
